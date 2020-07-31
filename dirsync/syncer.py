tkhyn/dirsync

commit 27efc727b05c45d96ddbaf8f32e98713de6fd660

fix(syncer): fix separator issue in _compare method
fix #22

dirsync/syncer.py
@@ -159,8 +159,10 @@ class Syncer(object):
                 if add_path:
                     left.add(path)
                     anc_dirs = re_path[:-1].split('/')
-                    for i in range(1, len(anc_dirs)):
-                        left.add('/'.join(anc_dirs[:i]))
+                    anc_dirs_path = ''
+                    for ad in anc_dirs:
+                        anc_dirs_path = os.path.join(anc_dirs_path, ad)
+                        left.add(anc_dirs_path)
 
         for cwd, dirs, files in os.walk(dir2):
             for f in dirs + files:

commit 4ca9b27828d62015cc4bbe1ff3e03cdf56774772

fix(syncer): fix range issue introduced in 27efc727

dirsync/syncer.py
@@ -160,7 +160,7 @@ class Syncer(object):
                     left.add(path)
                     anc_dirs = re_path[:-1].split('/')
                     anc_dirs_path = ''
-                    for ad in anc_dirs:
+                    for ad in anc_dirs[1:]:
                         anc_dirs_path = os.path.join(anc_dirs_path, ad)
                         left.add(anc_dirs_path)
 